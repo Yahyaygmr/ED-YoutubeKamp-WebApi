fce44a78d7fa6d58d856bf7557daa2ab4b215058
Yahya <YAHYA@DESKTOP-4QIIH5S>
2024-02-25 22:51:28 +0300

    committed

--- Business/Abstracts/IBrandService.cs
+++ Business/Abstracts/IBrandService.cs
@@ -11,7 +11,7 @@ namespace Business.Abstracts
 {
     public interface IBrandService
     {
-        CreatedBrandResponse Add(CreateBrandRequest brand);
-        List<Brand> GetAll();
+        CreatedBrandResponse Add(CreateBrandRequest createBrandRequest);
+        List<GetAllBrandResponse> GetAll();
     }
 }
